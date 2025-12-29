aafc5bc0475d09f2a7cb6e305a8b743a9d6a439e
0e-01 <01cx7ver1@gmail.com>
2025-12-29 23:10:38 +0800

    Refactor foodMessages and alert messages

--- script.js
+++ script.js
@@ -2,24 +2,12 @@ let money = Number(localStorage.getItem("money")) || 30;
 updateMoney();
 
 const foodMessages = {
-  pineapple: [
-    "鳳梨酸酸甜甜，好清爽！"
-  ],
-  noodle: [
-    "鐵板麵香氣逼人，太罪惡了！"
-  ],
-  takoyaki: [
-    "這簡直是章魚燒界的登峰造極！"
-  ],
-  sprite: [
-    "雪碧一喝，整個人都醒了！"
-  ],
-  tiramisu: [
-    "這個提拉米蘇簡直讚不絕口！"
-  ],
-  dumpling: [
-    "這個水餃志在必得，太好吃了！"
-  ]
+  pineapple: ["鳳梨酸甜剛剛好～"],
+  noodle: ["鐵板麵香氣四溢！"],
+  takoyaki: ["這簡直是章魚燒界的登峰造極！"],
+  sprite: ["雪碧喝下去超爽！"],
+  tiramisu: ["這個提拉米蘇簡直讚不絕口！"],
+  dumpling: ["這個水餃志在必得，太好吃了！"]
 };
 
 function updateMoney() {
@@ -31,18 +19,15 @@ function feed(price, type) {
   if (money >= price) {
     money -= price;
     updateMoney();
-
-    const msgs = foodMessages[type];
-    const msg = msgs[Math.floor(Math.random() * msgs.length)];
+    const msg = foodMessages[type][0];
     alert(msg);
   } else {
-    alert("錢不夠了，快去玩遊戲！");
+    alert("錢不夠，快去玩遊戲！");
   }
 }
 
 function changeClothes(type) {
-  const pet = document.getElementById("pet");
-  pet.className = "pet " + type;
+  document.getElementById("pet").className = "pet " + type;
 }
 
 function openGame() {
